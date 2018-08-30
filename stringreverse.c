7e1e32aa7abb523db45996584cc0d5d3fca6013e
ABHINAV <42680574+abhinavgla02@users.noreply.github.com>
2018-08-30 23:07:56 +0530

    Create stringreverse.c

---
+++ stringreverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+void main(){
+char a[20],b[20];
+int i,j,len;
+gets(a);
+j=0;
+len=strlen(a);
+for(i=len-1;i>=0;i--)
+{
+  b[j++]=a[i];
+}
+b[j]='\0';
+printf("%s",b);
+}
